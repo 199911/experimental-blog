355fcecc1838b28e47449855b76220e5a74be470
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-04-24 22:00:58 +0800

    Implement a very simple front page

--- src/pages/FrontPage.js
+++ src/pages/FrontPage.js
@@ -1,8 +1,49 @@
 import React from 'react'
+import 'isomorphic-fetch';
 
 class HelloMessage extends React.Component {
+
+  constructor() {
+  super();
+  this.state = {};
+}
+
+  componentDidMount() {
+    fetch('https://199911.github.io/blog-data/front-page.json')
+      .then((res) => (res.json()))
+      .then((posts) => {
+        this.setState({
+          posts
+        });
+      })
+  }
+
   render() {
-    return <div>Hello {this.props.name}</div>;
+    const { posts } = this.state;
+    if (posts) {
+      return (
+        <div>
+          {
+            posts.map((post) => {
+              return (
+                <div style={{ height: '300px', overflow: 'hidden' }}>
+                  <h1>
+                  {post.title}
+                  </h1>
+                  <div dangerouslySetInnerHTML={{ __html: post.__content }}>
+                  </div>
+                  <pre>
+                    { JSON.stringify(post, null, 4) }
+                  </pre>
+                </div>
+              );
+            })
+          }
+        </div>
+      )
+    } else {
+      return <div>Loading</div>;
+    }
   }
 }
 
