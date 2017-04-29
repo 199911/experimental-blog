601ece3e6976d340a89ac68b1a4642ffe3ad036d
Ku Lok Sun <sundaykuloksun@gmail.com>
2017-04-29 16:40:24 +0800

    Add react router

--- src/containers/Root.js
+++ src/containers/Root.js
@@ -1,8 +1,15 @@
 import React from 'react'
+import {
+    BrowserRouter as Router,
+    Route,
+    Link
+} from 'react-router-dom'
 import FrontPage from '../pages/FrontPage';
 
 const Root = () => (
-  <FrontPage />
+    <Router>
+        <Route exact path="/" component={FrontPage}/>
+    </Router>
 )
 
 export default Root
